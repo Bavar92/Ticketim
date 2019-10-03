7ae9c7c7685052096df53ac1f4ef54908baddda9
Bavar92 <vasilij6090@gmail.com>
2019-10-03 14:46:51 +0300

    add functions to homepage

---
+++ wp-content/themes/ticketim/single.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<?php gt_set_post_view(); ?>
+<section class="content">
+    <div class="container">
+        <article>
+            <h1><?php the_title(); ?></h1>
+            <?php
+            if ('' !== get_post()->post_content) { ?>
+                <div class="wysiwyg">
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <?php the_content(); ?>
+                    <?php endwhile; endif; ?>
+                </div>
+            <?php } ?>
+            <a class="button" href="<?php echo get_permalink(BLOG_ID); ?>">Go back</a>
+        </article>
+        <aside>
+            <?php dynamic_sidebar("Blog Sidebar") ?>
+        </aside>
+    </div>
+</section>
+
+<?php get_footer(); ?>
+
