6c38a620f6ba0447463c841e624b7f0f35929ac2
Bavar92 <vasilij6090@gmail.com>
2019-10-07 17:23:15 +0300

    +

--- wp-content/themes/ticketim/woocommerce/single-product.php
+++ wp-content/themes/ticketim/woocommerce/single-product.php
@@ -25,22 +25,32 @@ get_header('shop'); ?>
         <div class="wrap">
         </div>
     </div>
+<? } else { ?>
+    <div class="top-block cover" style="background-image: url('<?php echo theme() ?>/assets/placeholder-image.jpg')">
+        <div class="wrap">
+        </div>
+    </div>
 <? } ?>
 
-<?php
-
-$images = get_field('image_gallery');
-
-if ($images): ?>
     <div class="wrap mini">
         <div class="flex">
             <div class="single-text">
-                <h1><? the_title() ?></h1>
-                <? if ($product_type = get_field('product_type')) { ?>
-                    <div class="type">
-                        <?= $product_type ?>
-                    </div>
-                <? } ?>
+
+                <?php
+                /**
+                 * Hook: woocommerce_single_product_summary.
+                 *
+                 * @hooked woocommerce_template_single_title - 5
+                 * @hooked woocommerce_template_single_rating - 10
+                 * @hooked woocommerce_template_single_price - 10
+                 * @hooked woocommerce_template_single_excerpt - 20
+                 * @hooked woocommerce_template_single_add_to_cart - 30
+                 * @hooked woocommerce_template_single_meta - 40
+                 * @hooked woocommerce_template_single_sharing - 50
+                 * @hooked WC_Structured_Data::generate_product_data() - 60
+                 */
+                do_action('woocommerce_single_product_summary');
+                ?>
                 <?
 
                 if (get_field('from_date') || get_field('to_date')) { ?>
@@ -50,37 +60,87 @@ if ($images): ?>
                 <? } ?>
             </div>
             <div class="gallery-images flex">
-                <div class="image-mini">
-                    <?php foreach ($images as $image): ?>
-                        <div class="image">
-                            <img src="<?php echo $image['sizes']['gallery']; ?>" alt="<?php echo $image['alt']; ?>"/>
-                            <p><?php echo $image['caption']; ?></p>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-
-                <div class="image-big">
-                    <?php foreach ($images as $image): ?>
-                        <div class="image">
-                            <img src="<?php echo $image['sizes']['gallery']; ?>" alt="<?php echo $image['alt']; ?>"/>
-                            <p><?php echo $image['caption']; ?></p>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-
+                <?php
+                $images = get_field('image_gallery');
+                if ($images): ?>
+                    <div class="image-mini">
+                        <?php foreach ($images as $image): ?>
+                            <div class="image">
+                                <img src="<?php echo $image['sizes']['gallery']; ?>"
+                                     alt="<?php echo $image['alt']; ?>"/>
+                                <p><?php echo $image['caption']; ?></p>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="image-big">
+                        <?php foreach ($images as $image): ?>
+                            <div class="image">
+                                <img src="<?php echo $image['sizes']['gallery']; ?>"
+                                     alt="<?php echo $image['alt']; ?>"/>
+                                <p><?php echo $image['caption']; ?></p>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
             </div>
-
         </div>
+        <? if ($trip = get_field('trip_t')) { ?>
+            <div class="trip style-custom-single">
+                <? foreach ($trip as $tr) { ?>
+                    <h2><?= $tr['type_trip'] ?></h2>
+                    <div class="wrapper flex">
+                        <div class="directions half alc">
+                            <p><?= $tr['date'] ?></p>
+                            <p><?= $tr['from_city'] ?></p>
+                            <p><?= $tr['to_city'] ?></p>
+                        </div>
+                        <? if ($flight = $tr['flight']) { ?>
+                            <div class="flight half">
+                                <? foreach ($flight as $f) { ?>
+                                    <div class="flight-info">
+                                        <div class="header alc">
+                                            <p><strong>וודר בלונדון</strong></p>
+                                            <p><?= $tr['date'] ?></p>
+                                            <p>וודר בלונדון</p>
+                                            <p><?= $f['flight_time'] ?></p>
+                                        </div>
+                                        <div class="body alc">
+                                            <div class="company col-3">
+                                                <? $image = $f['company_icon'] ?>
+                                                <img src="<?= $image['sizes']['medium'] ?>" alt="<?= $image['alt'] ?>">
+                                                <p><?= $f['company_name'] ?></p>
+                                            </div>
+                                            <div class="departure col-3">
+                                                <div class="alc">
+                                                    <p class="abbr"><?= $f['departure_city'] ?></p>
+                                                    <p><?= $f['departure_time'] ?></p>
+                                                </div>
+                                                <p><?= $f['departure_airport'] ?></p>
+                                            </div>
+                                            <div class="arrival col-3">
+                                                <div class="alc">
+                                                    <p class="abbr"><?= $f['arrival_city'] ?></p>
+                                                    <p><?= $f['arrival_time'] ?></p>
+                                                </div>
+                                                <p><?= $f['arrival_airport'] ?></p>
+                                            </div>
+                                            <div class="code col-3">
+                                                <p><strong>Code</strong></p>
+                                                <p><?= $f['airplane_number'] ?></p>
+                                            </div>
+                                        </div>
+                                        <div class="bottom-info"><?= $f['short_description'] ?></div>
+                                    </div>
+                                <? } ?>
+                            </div>
+                        <? } ?>
+                    </div>
+                <? } ?>
+            </div>
+        <? } ?>
 
     </div>
-<?php endif; ?>
 
-
-<?php while (have_posts()) : the_post(); ?>
-
-    <?php wc_get_template_part('content', 'single-product'); ?>
-
-<?php endwhile; // end of the loop. ?>
 
 <?php
 /**
@@ -89,15 +149,6 @@ if ($images): ?>
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
 do_action('woocommerce_after_main_content');
-?>
-
-<?php
-/**
- * woocommerce_sidebar hook.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action('woocommerce_sidebar');
 ?>
 
 <?php get_footer('shop');
